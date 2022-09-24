2839cb40a2db03cc9c091c2f5a05d41de051df44
migantoju <a_niupi_byy@hotmail.com>
2022-09-23 20:51:29 -0500

    [FEAT] Implemented opcodes from 0x8 to 0xC.

--- cmd/chip8/main.go
+++ cmd/chip8/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -85,7 +87,7 @@ type CPU struct {
 	Memory [4096]byte   // memory ram
 	Opcode uint16       // current opcode
 	I      uint16       // Index stack - (rightmost) 12 bits are usually used
-	Vx     [16]uint8    // Vx registers
+	V      [16]uint8    // Vx registers
 	PC     uint16       // Program Counter
 	SP     uint16       // Stack Pointer
 	S      [16]uint16   // Stack
@@ -122,7 +124,11 @@ func (c *CPU) Emulate() {
 		case 0x00E0:
 			fmt.Println(".....Clearing the screen.....")
 			// clear screen
-
+			for i := 0; i < len(c.Vr); i++ {
+				for j := 0; j < len(c.Vr[i]); j++ {
+					c.Vr[i][j] = 0x0
+				}
+			}
 			// increment the PC by two
 			c.PC += 2
 			break
@@ -137,89 +143,219 @@ func (c *CPU) Emulate() {
 
 			c.PC += 2
 			break
-		case 0x1000:
-			// Jump to location nnn
-			// The interpreter sets the program counter to nnn
-			c.PC = c.Opcode & 0x0FFF
+		default:
+			fmt.Printf("Invalid opcode %x", c.Opcode)
+		}
+	case 0x1000:
+		// Jump to location nnn
+		// The interpreter sets the program counter to nnn
+		c.PC = c.Opcode & 0x0FFF
+		break
+	case 0x2000:
+		// Call subroutine at nnn
+		// The interpreter increments the Stack Pointer
+		c.SP++
+		// Then, puts the current PC on the top of the stack.
+		c.S[c.SP] = c.PC
+		// The PC is then set to nnn
+		c.PC = c.Opcode & 0x0FFF
+
+		break
+	case 0x3000:
+		// Skip the next instruction if Vx = kk
+
+		// The interpreter compares register Vx to kk,
+		// and if they are equal, increments the program
+		// counter by 2.
+		x := (c.Opcode & 0x0F00) >> 8
+		kk := byte(c.Opcode)
+
+		if c.V[x] == kk {
+			c.PC += 2
+		}
+
+		c.PC += 2
+
+		break
+	case 0x4000:
+		// Skip the next instruction if Vx != kk
+
+		// The interpreter compares register Vx to kk
+		// if they are not equal, increments the PC by 2.
+		x := (c.Opcode & 0x0F00) >> 8
+		kk := byte(c.Opcode)
+
+		if c.V[x] != kk {
+			c.PC += 2
+		}
+
+		c.PC += 2
+
+		break
+	case 0x5000:
+		// Skip next instruction if Vx = Vy
+		x := (c.Opcode & 0x0F00) >> 8
+		y := (c.Opcode & 0x00F0) >> 4
+
+		c.PC += 2
+
+		if c.V[x] == c.V[y] {
+			c.PC += 2
+		}
+
+		break
+	case 0x6000:
+		// set Vx = kk
+		// The interpreter puts the value kk into
+		// register Vx
+		x := (c.Opcode & 0x0F00) >> 8
+		kk := byte(c.Opcode)
+
+		c.V[x] = kk
+
+		c.PC += 2
+
+		break
+	case 0x7000:
+		// set Vx = Vx + kk
+
+		// Adds the value kk to the value of register Vx
+		// then stores the result in Vx
+		x := (c.Opcode & 0x0F00) >> 8
+		kk := byte(c.Opcode)
+
+		c.V[x] = c.V[x] + kk
+
+		c.PC += 2
+		break
+	case 0x8000:
+		x := (c.Opcode & 0x0F00) >> 8
+		y := (c.Opcode & 0x00F0) >> 4
+		switch c.Opcode & 0x000F {
+		case 0x0000:
+			// set Vx = Vy
+			c.V[x] = c.V[y]
+			c.PC += 2
 			break
-		case 0x2000:
-			// Call subroutine at nnn
-			// The interpreter increments the Stack Pointer
-			c.SP++
-			// Then, puts the current PC on the top of the stack.
-			c.S[c.SP] = c.PC
-			// The PC is then set to nnn
-			c.PC = c.Opcode & 0x0FFF
-
+		case 0x0001:
+			// Set Vx = Vx or Vy
+			c.V[x] = c.V[x] | c.V[y]
+			c.PC += 2
 			break
-		case 0x3000:
-			// Skip the next instruction if Vx = kk
+		case 0x0002:
+			// Set Vx = Vx AND Vy
+			c.V[x] = c.V[x] & c.V[y]
+			c.PC += 2
+			break
+		case 0x0003:
+			// Set Vx = Vx XOR Vy
+			c.V[x] = c.V[x] ^ c.V[y]
+			c.PC += 2
+			break
+		case 0x0004:
+			// Set Vx = Vx + Vy, set VF = carry
 
-			// The interpreter compares register Vx to kk,
-			// and if they are equal, increments the program
-			// counter by 2.
-			x := (c.Opcode & 0x0F00) >> 8
-			kk := byte(c.Opcode)
+			// The values of Vx and Vy are added together.
+			result := uint16(c.V[x]) + uint16(c.V[y])
 
-			if c.Vx[x] == kk {
-				c.PC += 2
+			// init to zero value
+			var carry byte
+
+			// If the resylt is greater than 8 bits VF is set to 1
+			// otherwise 0.
+			if result > 0xFF {
+				carry = 1
 			}
+			c.V[0xF] = carry
+
+			// only the lowest 8 bits of the result are kept,
+			// and stored in Vx
+			c.V[x] = byte(result)
 
 			c.PC += 2
-
 			break
-		case 0x4000:
-			// Skip the next instruction if Vx != kk
+		case 0x0005:
+			// Set Vx = Vx - Vy, set VF = NOT borrow
 
-			// The interpreter compares register Vx to kk
-			// if they are not equal, increments the PC by 2.
-			x := (c.Opcode & 0x0F00) >> 8
-			kk := byte(c.Opcode)
-
-			if c.Vx[x] != kk {
-				c.PC += 2
+			if c.V[x] > c.V[y] {
+				c.V[0xF] = byte(1)
 			}
+			c.V[0xF] = byte(0)
 
+			c.V[x] = c.V[x] - c.V[y]
 			c.PC += 2
-
 			break
-		case 0x5000:
-			// Skip next instruction if Vx = Vy
-			x := (c.Opcode & 0x0F00) >> 8
-			y := (c.Opcode & 0x00F0) >> 4
+		case 0x0006:
+			// Set Vx = Vx SHR 1
+			var carry byte
 
-			c.PC += 2
-
-			if c.Vx[x] == c.Vx[y] {
-				c.PC += 2
+			if (c.V[x] & 0x01) == 0x01 {
+				carry = 1
 			}
-
-			break
-		case 0x6000:
-			// set Vx = kk
-			// The interpreter puts the value kk into
-			// register Vx
-			x := (c.Opcode & 0x0F00) >> 8
-			kk := byte(c.Opcode)
-
-			c.Vx[x] = kk
-
+			c.V[0xF] = carry
+			c.V[x] = c.V[x] / 2
 			c.PC += 2
-
 			break
-		case 0x7000:
-			// set Vx = Vx + kk
+		case 0x0007:
+			// Set Vx = Vy - Vx, set VF = NOT borrow
+			var carry byte
 
-			// Adds the value kk to the value of register Vx
-			// then stores the result in Vx
-			x := (c.Opcode & 0x0F00) >> 8
-			kk := byte(c.Opcode)
+			if c.V[y] > c.V[x] {
+				carry = 1
+			}
+			c.V[0xF] = carry
+			c.V[x] = c.V[y] - c.V[x]
+			c.PC += 2
+			break
+		case 0x000E:
+			// Set Vx = Vx SHL 1
+			var carry byte
 
-			c.Vx[x] = c.Vx[x] + kk
-
+			if (c.V[x] & 0x01) == 0x01 {
+				carry = 1
+			}
+			c.V[0xF] = carry
+			c.V[x] = c.V[x] * 2
 			c.PC += 2
 			break
 		}
+	case 0x9000:
+		x := (c.Opcode & 0x0F00) >> 8
+		y := (c.Opcode & 0x00F0) >> 4
+		switch c.Opcode & 0x000F {
+		case 0x0000:
+			if c.V[x] != c.V[y] {
+				c.PC += 2
+			}
+
+			c.PC += 2
+			break
+		default:
+			fmt.Printf("Invalid Opcode: %X", c.Opcode)
+		}
+	case 0xA000:
+		// Set I = nnn
+		// The value of register I is set to nnn
+		c.I = c.Opcode & 0x0FFF
+		c.PC += 2
+		break
+	case 0xB000:
+		// Jump to location nnn + V0
+		// The program counter is set to nnn plus the value of V0
+		c.PC = c.Opcode&0x0FFF + uint16(c.V[0x0])
+		break
+	case 0xC000:
+		// Set Vx = random byte AND kk
+		x := (c.Opcode & 0x0F00) >> 8
+		kk := byte(c.Opcode)
+
+		c.V[x] = kk + randomByte()
+		c.PC += 2
+		break
+	case 0xD000:
+		// Display n-byte sprite starting at memory location
+		// I at (Vx, Vy), set VF=collision.
+
 	}
 }
 
@@ -230,4 +366,13 @@ func (c *CPU) GetOpcode() {
 	fmt.Println("Fetching the next opcode.")
 
 	c.Opcode = uint16(c.Memory[c.PC]<<8) | uint16(c.Memory[c.PC+1])
+}
+
+func (c *CPU) LoadRom(rom []byte) (int, error) {
+	reader := bytes.NewReader(rom)
+	return reader.Read(c.Memory[0x200:])
+}
+
+func randomByte() byte {
+	return byte(rand.New(rand.NewSource(time.Now().UnixNano())).Intn(255))
 }
